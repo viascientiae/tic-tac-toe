e9c8c4bd5eb421e01932e44187c7c67280a303d8
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-12-17 21:19:25 +0530

    Implement Game Logic for Two-Player Game
    - Added a Game class to handle the gameplay logic.
    - The Game class includes methods for playing the game, prompting moves,
     updating the board, and checking game status (win/draw).
    - Implemented a game loop within the Game class to manage the flow of
    the game.
    - Added logic for switching players and determining the game outcome.
    - Integrated the Player and Board classes within the Game class.
    - Included user interaction for starting the game and making moves.
    - This commit establishes the core functionality for a two-player board
     game.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -26,3 +26,89 @@ class Board
   end
 
 end
+
+class Game
+
+  attr_reader :player1, :player2, :board
+
+  WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @board = Board.new
+    @current_player = @player1
+  end
+
+  def play
+    loop do
+      @board.display
+      position = prompt_move
+      next unless update_board(position)
+      break if game_over?
+      switch_player
+      end
+     display_result
+    end
+
+  def prompt_move
+    puts "#{@current_player.name}, please place your token #{@current_player.token} on an empty cell in the board."
+    gets.chomp.to_i
+  end
+
+  def update_board(position)
+    if (1..9).include?(position) && (1..9).include?(@board.board[position-1])
+      @board.board[position-1] = @current_player.token
+      true
+    else
+      puts "Invalid move. Please try again."
+      false
+    end
+  end
+
+  def game_over?
+    win? || draw?
+  end
+
+  def switch_player
+    @current_player = @current_player == @player1 ? @player2 : @player1
+  end
+
+  def win?
+    WINNING_COMBINATIONS.any? do |combination|
+      combination.map { |index| @board.board[index] }.uniq == [@current_player.token]
+    end
+  end
+
+  def draw?
+    !@board.board.any? { |cell| cell.is_a?(Integer) } && !win?
+  end
+
+  def display_result
+    if win?
+      @board.display
+      puts "Congratulations #{@current_player.name}, you've won the game!"
+    elsif draw?
+      @board.display
+      puts "The game is draw."
+    end
+  end
+
+end
+
+  puts "Enter name of Player 1"
+  player1_name= gets.chomp
+  player1 = Player.new(player1_name, "X")
+  puts "Welcome #{player1.name}, you'll be playing #{player1.token} token"
+
+  puts "Enter name of Player 2"
+  player2_name = gets.chomp
+  player2 = Player.new(player2_name, "O")
+  puts "Welcome #{player2.name}, you'll be playing #{player2.token} token"
+
+  new_game = Game.new(player1, player2)
+  new_game.play
